51a71efc6c495c122238357fc77cf415352ae396
Arthur Leonard Andersen <aandersen@velalu.qa>
2018-03-01 20:28:29 +0100

    Remove education/entertainment/medical, add health/pension constants

--- lib/constants.rb
+++ lib/constants.rb
@@ -1,7 +1,7 @@
 module Constants
   CURRENT_ACCOUNTS = 'Assets:(Cash|Fidor|DKB|PayPal)'
   LONG_TERM_FUNDS  =
-    'Assets:Funds:(Emergency|Travel|Legal|Living:Household|Auto|Bike|Education|Entertainment|Medical|Clothes|Gifts|Investable)'
+    'Assets:Funds:(Emergency|Travel|Legal|Living:Household|Auto|Bike|Health|Clothes|Gifts|Investable|Pension)'
   LONG_TERM_LIABILITIES = 'Liabilities:(Loans)'
   LIQUID_TICKERS   = 'VASIX|\$'
 end
